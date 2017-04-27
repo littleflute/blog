ee43d5489f5789475cf1db5b954e39590bc70c4d
littleflute <13699175164@sina.cn>
2017-04-27 14:33:18 -0500

    Add files via upload

--- docs/2017/04/22/0422.js
+++ docs/2017/04/22/0422.js
@@ -8,8 +8,7 @@ var eXdMouseDown	=	2;
 addHandler("myDiv",eXdMouseMove); 
 
 var myKLines		= {
-
-	v			: "0.0.1",
+	b			: "0.0.1",
 	maxX		: 0,
 	minX		: 0,	
 	maxY		: 0,
@@ -17,7 +16,8 @@ var myKLines		= {
 	numAll		: 0,
 	maxVal		: 0,
 	minVal		: 0,
-	kLines		: "1275,1279,1270,1274;",
+	kLines		: "",//1275,1279,1270,1274;",
+	DataFromSrc	: "",
 	
 	xdPush		: function(o,h,l,c)
 	{
@@ -65,10 +65,21 @@ var myKLines		= {
 	{
 		var sRet	= "";
 		var a		= this.kLines.split(";");
-		
+		var x		= 50;
 		for(var i = 0; i<a.length-1; i++)
 		{
 			var ohlc = a[i].split(",");
+			var oY = this.getYByVal(ohlc[0]);
+			var hY = this.getYByVal(ohlc[1]);
+			var lY = this.getYByVal(ohlc[2]);
+			var cY = this.getYByVal(ohlc[3]);
+			var dXX	= 10;
+			x		+= dXX;
+			var h = lY - hY;
+			if(hY>0)
+			{
+				this.draw_1_KLine(eID,x,oY,hY,lY,cY);
+			}
 			sRet += a[i];
 			sRet += "    :    "+ this.getYByVal(ohlc[0]) + "-";
 			sRet += this.getYByVal(ohlc[1]) + "-";
@@ -78,7 +89,7 @@ var myKLines		= {
 		}
 		return sRet;
 	},
-	drawAll		: function(eID)
+	drawFrame	: function(eID)
 	{
 		var p				= document.getElementById(eID);
 		var o				= document.createElement("div");
@@ -88,20 +99,106 @@ var myKLines		= {
 		o.style.left		= "20px";
 		o.style.top			= this.minY + "px";
 		o.style.height		= this.maxY - this.minY+"px";
-		o.style.color		= "yellow";
-		o.innerHTML			= this.parseKLines(eID);
+		 
 		p.appendChild(o);
-	}
+	},
+	draw_1_KLine	: function(eID,x,oY,hY,lY,cY)
+	{
+		var p				= document.getElementById(eID);
+		var oHightLow		= document.createElement("div");
+		var	hh				= 0;
+		var d			= 2;
+		var y;
+		var color;
+		
+		if(cY > oY)
+		{
+			y		= oY;
+			hh		= cY - oY;
+			color	= "green";
+		}  
+		if(cY <= oY)
+		{
+			y		= cY;
+			hh		= oY - cY;
+			hh		= hh?hh:1;
+			color	= "red";
+		}
+		oHightLow.style.position	= "absolute";
+		oHightLow.style.border		= "0px blue solid";
+		oHightLow.style.width		= "1px";
+		oHightLow.style.left		= x + "px";
+		oHightLow.style.top			= hY + "px";
+		oHightLow.style.height		= lY-hY + "px";
+		oHightLow.style.background	= color;
+		p.appendChild(oHightLow);
+
+		
+		var oOpenClose	= document.createElement("div");
+		
+		oOpenClose.style.position	= "absolute";
+		oOpenClose.style.border		= "0px blue solid";
+		oOpenClose.style.width		= "5px";
+		oOpenClose.style.left		= x - d + "px";
+		oOpenClose.style.top		= y + "px";
+		oOpenClose.style.height		= hh + "px";
+		oOpenClose.style.background	= color; 
+		p.appendChild(oOpenClose);
+
+	},
+	getDataFromSrc : function (src)
+	{
+		var xmlhttp;
+		if (window.XMLHttpRequest)
+		{//"code for IE7+, Firefox, Chrome, Opera, Safari"
+			xmlhttp = new XMLHttpRequest();
+		}
+		else
+		{// code for IE6, IE5
+			xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+		}
+		xmlhttp.onreadystatechange=function()
+		{
+			if (xmlhttp.readyState==4 && xmlhttp.status==200)
+			{
+				myKLines.getData(xmlhttp.responseText);
+			}
+			}
+		xmlhttp.open("GET",src,true);
+		xmlhttp.send();
+	},
+	getData		: function(sData)
+	{
+		var a		= sData.split(";");
+	 
+		for(var i = 0; i<a.length; i++)
+		{
+			var d	= a[i].split(" ");
+			if(!(d[1]=="undefined" || d[1]==null))
+			{
+				var ohlc = d[1].split(",");
+				this.xdPush(ohlc[0],ohlc[1],ohlc[2],ohlc[3]);
+			}
+		}
+		 
+
+		this.drawAll("myDiv");
+
+	},
+	drawAll		: function(eID)
+	{
+		this.drawFrame(eID);
+		this.parseKLines(eID);
+	},
+	e : "0.0.2" 
 }//	___myKlinesEnd:"2017.04.27 9:25am";
 
 myKLines.setMaxMinVal(1295,1195);
 myKLines.setMaxMinXY(100,280,10,10);
-myKLines.xdPush(1268,1275,1265,1269);
-myKLines.xdPush(1277,1279,1265,1271);
-myKLines.xdPush(1267,1275,1265,1260);
-myKLines.xdPush(1295,1295,1265,1270);
-myKLines.xdPush(1195,1275,1195,1270);
-myKLines.drawAll("myDiv");
+myKLines.getDataFromSrc("http://www.beautifullover.org/php/xau-days.txt");
+
+
+
   
 var myVer1Ctx = { 
     id			: "myVer1",
@@ -252,3 +349,18 @@ function myDivMousemove(e,x){
     tt.innerHTML	= myX;
     
 }
+/*
+xau-days.txt
+20170419 1289.85,1290.95,1274.14,1280.05;
+20170420 1280.72,1283.48,1276.75,1281.53;
+20170423 1281.80,1288.45,1278.56,1284.47;
+20170424 1275.67,1277.65,1265.64,1275.54;
+20170425 1276.51,1278.35,1261.71,1263.88;
+20170426 1264.42,1270.70,1260.15,1269.03;
+
+
+
+
+
+
+*/
