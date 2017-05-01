ff5e21d47b4cc6c8e36539734ad4eee3ecf565cf
littleflute <13699175164@sina.cn>
2017-05-01 18:22:23 -0500

    Add files via upload

---
+++ xd.js
@@ -0,0 +1,50 @@
+var x = document.getElementsByTagName("h1")[0];
+x.innerHTML = "LittleFlute's Blog V0.4.0";
+var b	= document.getElementById("banner");
+var s = b.innerHTML;
+s += "<a href='docs' class='button downloads'>docs</a>";
+s += "<a href='html' class='button downloads'>html</a>";
+b.innerHTML = s;
+
+
+var gN   = 0;
+var gStr = "";
+function songSrc(n)
+{
+	gN = n;
+    var s = "https://littleflute.github.io/blog/html/songs/s00" + n + ".html";
+    return s;
+}
+function loadDoc(src) {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      var str = this.responseText;
+   //   document.getElementById("demo").innerHTML =str;
+      
+      //
+      var pos = str.indexOf("scale=1.165199");
+      
+      document.getElementById("debug").innerHTML = pos;
+      if(pos !=-1)
+      {
+         gStr += gN;
+         gStr += ": ";
+         gStr += "<a href=' ";
+		 gStr += src;
+         gStr += "'> ";
+		 gStr += gN;
+		 gStr += "</a>";
+         gStr += "<br>";
+         document.getElementById("demo").innerHTML = gStr;
+		 loadDoc(songSrc(gN+1));
+
+      }
+     //*/
+    }
+  };
+  xhttp.open("GET", src, true);
+  xhttp.send();
+}
+ 
+loadDoc(songSrc(1));
