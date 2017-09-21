860243ed8fce26ed736a22ce530d19f1b6ae146e
littleflute <13699175164@sina.cn>
2017-09-21 11:26:23 -0700

    Update DrawKlines.js

--- trading/js/DrawKlines.js
+++ trading/js/DrawKlines.js
@@ -1,3 +1,7 @@
-//file: DrawKlines.js v0.0.2
+//file: DrawKlines.js v0.0.3
 //by LittleFlute
 //10:07 AM 9/21/2017
+
+function myFunction() {
+    document.getElementById("DivTestContend").innerHTML="test myFunction().";
+}
