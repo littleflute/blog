6623035fb2fb13e05ca9dddc60e19ef6d542f6bc
littleflute <13699175164@sina.cn>
2017-09-30 03:02:26 +0800

    Update blDraggableDIV.js

--- js/blDraggableDIV.js
+++ js/blDraggableDIV.js
@@ -1,2 +1,7 @@
-var v_blDraggableDIV = "v0.0.1";
-alert(v_blDraggableDIV);
+var v_blDraggableDIV = "v0.0.3";
+var i2do  ="DraggableDIV";
+var s = v_blDraggableDIV;
+s += "\r\n";
+s += i2do;
+
+alert(s);
