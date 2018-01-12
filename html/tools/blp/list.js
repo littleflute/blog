7ac74e752ed229e13b3023273df7239f18f309d0
littleflute <13699175164@sina.cn>
2018-01-12 13:42:14 +0800

    Update list.js

--- html/tools/blp/list.js
+++ html/tools/blp/list.js
@@ -1,5 +1,10 @@
-var s = "<div id='id_div_list_toolbar'></div>";
-var listUI= blo0.blMDiv(document.body,"id_mdiv_list5",s,50,200,500,800,blColor[5]); 
+var v = "v0.0.2"; 
+var s0 = '<div id="id_div_list_toolbar">';
+ s0 += '<a target="_blank" href="list.js">' + v + '</a>-';
+  s0 += '<a target="_blank" href="https://github.com/littleflute/blog/edit/master/html/tools/blp/list.js">' + v + '*</a>';
+  s0 += '</div>';
+
+var listUI= blo0.blMDiv(document.body,"id_mdiv_list5",s0,50,200,500,800,blColor[5]); 
 listUI.tb = bl$('id_div_list_toolbar');
 listUI.tb.b1 = blo0.blBtn(listUI.tb,listUI.tb.id+"b1","b1","grey"); 
 listUI.tb.b1.onclick = function(){
