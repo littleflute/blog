21797bc8047866a380d5433fb21e6f48ca7adafc
littleflute <13699175164@sina.cn>
2018-01-12 12:41:51 +0800

    Update blp.js

--- html/tools/blp/blp.js
+++ html/tools/blp/blp.js
@@ -1,2 +1,2 @@
-var v = "v0.0.14" 
-blo0.blMDiv(document.body,"id_mdiv_blplayer","blplayer::",100,500,500,800,blGrey[0]);  
+var v = "v0.0.15" 
+blo0.blMDiv(document.body,"id_mdiv_blplayer","blplayer::",100,500,500,800,blColor[0]);  
