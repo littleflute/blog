05250b757ec75bc6062d378c1af601628bd422a9
littleflute <13699175164@sina.cn>
2018-01-12 13:23:56 +0800

    Update blp.js

--- html/tools/blp/blp.js
+++ html/tools/blp/blp.js
@@ -1,5 +1,5 @@
-var v = "v0.0.21" 
-var dUI = blo0.blMDiv(document.body,"id_mdiv_blplayer","blplayer::",500,300,500,800,blColor[1]);  
+var v = "v0.0.22" 
+var dUI = blo0.blMDiv(document.body,"id_mdiv_blplayer","blplayer::",650,200,500,800,blColor[1]);  
 dUI.d1 = blo0.blDiv(dUI,dUI.id+"d1","d1",blGrey[0]);
 
 dUI.d1.ta = blo0.blTextarea(dUI.d1, "id_ta_4_run" , "alert(1);" , blGrey[3]);
