a91ed3db3d0fb574a51f6eea8cd665da766116e5
littleflute <13699175164@sina.cn>
2018-01-12 12:35:56 +0800

    Update blp.js

--- html/tools/blp/blp.js
+++ html/tools/blp/blp.js
@@ -1,3 +1,3 @@
-var v = "v0.0.4"
-alert(blo0);
-blo0.blMDiv(document.body,"id_mdiv_blplayer","blplayer::",100,100,100,100,blGrey[0]); 
+var v = "v0.0.5" 
+blo0.blMDiv(document.body,"id_mdiv_blplayer","blplayer::",100,100,100,800,blGrey[0]); 
+blo0.blMDiv(document.body,"id_blClassDiv_4_wrap2","md2",800,10,500,200,"gray");
