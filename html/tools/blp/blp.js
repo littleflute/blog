f942d59893a83196aba8bd3d2deb078a4d68b98a
littleflute <13699175164@sina.cn>
2018-01-12 12:51:45 +0800

    Update blp.js

--- html/tools/blp/blp.js
+++ html/tools/blp/blp.js
@@ -1,11 +1,11 @@
-var v = "v0.0.16" 
-var dUI = blo0.blMDiv(document.body,"id_mdiv_blplayer","blplayer::",100,500,500,800,blColor[1]);  
+var v = "v0.0.21" 
+var dUI = blo0.blMDiv(document.body,"id_mdiv_blplayer","blplayer::",500,300,500,800,blColor[1]);  
 dUI.d1 = blo0.blDiv(dUI,dUI.id+"d1","d1",blGrey[0]);
 
 dUI.d1.ta = blo0.blTextarea(dUI.d1, "id_ta_4_run" , "alert(1);" , blGrey[3]);
 dUI.d1.ta.style.width = "98%";
 dUI.d1.ta.style.height = "200px";
-dUI.d1.btnRun = blo0.blBtn(d,"id_btn_blDesignClass_show","show","gray");
+dUI.d1.btnRun = blo0.blBtn(dUI.d1,"id_btn_run","run","green");
 dUI.d1.btnRun.onclick = function(){
   eval(dUI.d1.ta.value);
 }
