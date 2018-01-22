ec585b4ec9e7713176e089425f2370a219f1810d
littleflute <13699175164@sina.cn>
2018-01-22 14:04:24 +0800

    Update blVexTabDiv.js

--- html/songs/js/blVexTabDiv.js
+++ html/songs/js/blVexTabDiv.js
@@ -1,1 +1,19 @@
 
+var md = blo0.blMDiv(document.body,"id_mdiv_4_vextab","md_v0.0.42",50,50,900,200,blColor[4]); 
+var d1 = blo0.blDiv(md,md.id+"d1","d1",blGrey[1]);
+var s = "";
+		s += "<a target='_blank' href='js/blVexTabDiv.js'"
+		s += " style='color:green;'";
+		s +=">";
+		s += " blVexTabDiv.js ";
+		s += "</a>"; 
+		s += "<a target='_blank' href='https://github.com/littleflute/blog/edit/master/html/songs/js/blVexTabDiv.js'"
+		s += " style='color:blue;'";
+		s +=">";
+		s += " blVexTabDiv.js* ";
+		s += "</a>"; 
+
+var s1 = '<div id="div4Vextab" style="border:1px blue solid" class="vex-tabdiv" width=680 scale=1.0 editor="true" editor_width=680 editor_height=130>xd1</div>';
+d1.innerHTML = s + s1;
+
+bl$('div4Vextab').innerHTML = bl$('myID').innerHTML;
